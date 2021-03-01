305f44dc2e7b97a24311e01ba484a31f0e9e927e
FTITOR <francisco.ramirez@fr8hub.com>
2021-02-28 22:33:00 -0600

    ciclo de vida de una clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+  
+  constructor() {
+    // nacimiento
+    console.log('1. nacimiento');
+  }
+
+  // crece
+  ngOnInit() {
+    console.log('2. crecimiento');
+  }
+
+  // se reproduce
+  ngAfterViewInit() {
+    console.log('3. se reproduce');
+  }
+
+  // muere
+  ngOnDestroy() {
+    debugger;
+    console.log('4. muere');
+  }
+
+  // declaracion de variables
   title = 'meetup-angular';
+
 }
