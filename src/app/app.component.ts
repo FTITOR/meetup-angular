cbe7d5cd1952b0f2fc3ffa1081d818811e96120f
FTITOR <francisco.ramirez@fr8hub.com>
2021-03-05 23:53:20 -0600

    comentarios en el app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,49 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root',                  // Nombre que recibira el componente en el template: <app-component></app-component>
+  templateUrl: './app.component.html',   // path del template que empleara este component
+  styleUrls: ['./app.component.scss']    // path de la hoja de estilos que empleara este component
 })
-export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
-  
+export class AppComponent implements OnChanges, OnInit, AfterViewInit, OnDestroy { // OnChanges, OnInit, AfterViewInit, OnDestroy son interfaces que implementa la clase para tener un comportamiento
+
+  // Declaración de VARIABLES. Es importante declarar todas las variables publicas y privadas  
+  // en la parte superior de la clase, con la finalidad de tener un orden en nuestro código.
+
+  // Variables
+  public title = 'meetup-angular'; // Forma para declarar una variable publica
+  private id: number = 0;          // Forma para declarar una variable privada
+
+  // un constructor es donde se inicializa un objeto,
+  // aca se asignan los valores iniciales del nuevo objeto.
+  // (ES LO PRIMERO QUE SE EJECUTA AL INICIAR LA CLASE)
   constructor() {
-    // nacimiento
-    console.log('1. nacimiento');
+    // ---> PRIMERO EN EJECUTARSE CRONOLOGICAMENTE
+    console.log(`%c BY: ${DEVELOPER} FROM: app-component; ID: ${this.id++}. constructor()`, STYLES_FOR_LOGS);
   }
 
-  // crece
   ngOnInit() {
-    console.log('2. crecimiento');
+    // ---> SEGUNDO EN EJECUTARSE CRONOLOGICAMENTE
+    console.log(`%c BY: ${DEVELOPER} FROM: app-component; ID: ${this.id++}. ngOnInit()`, STYLES_FOR_LOGS);
   }
 
-  // se reproduce
   ngAfterViewInit() {
-    console.log('3. se reproduce');
+    // ---> TERCERO EN EJECUTARSE CRONOLOGICAMENTE
+    console.log(`%c BY: ${DEVELOPER} FROM: app-component; ID: ${this.id++}. ngAfterViewInit()`, STYLES_FOR_LOGS);
   }
 
-  // muere
+  ngOnChanges() {
+    // ---> SE EJECUTA CUANDO LOS PARAMETROS DE ENTRADA (INPUTS) DE LA CLASE CAMBIAN DE VALOR
+    console.log(`%c BY: ${DEVELOPER} FROM: app-component; ID: ${this.id++}. ngOnChanges()`, STYLES_FOR_LOGS);
+  }
+
   ngOnDestroy() {
-    debugger;
-    console.log('4. muere');
-  }
-
-  // declaracion de variables
-  title = 'meetup-angular';
+    // ---> SE EJECUTA CUANDO SE FINALIZA LA CLASE
+    console.log(`%c BY: ${DEVELOPER} FROM: app-component; ID: ${this.id++}. ngOnDestroy()`, STYLES_FOR_LOGS);
+  }  
 
 }
+
+// Constantes
+export const DEVELOPER: string = 'FTITOR';
+export const STYLES_FOR_LOGS = 'background: #000000 ; color: #f2ff00';
