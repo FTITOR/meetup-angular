39e9b9c1975d2ad2762d85deb82d547dd5f2af6f
FTITOR <francisco.ramirez@fr8hub.com>
2021-03-15 22:45:24 -0600

    app routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+
+const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'user-list', component: UserListComponent },
+  { path: 'user-detail', component: UserDetailComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
