e67c83af717a7a8d9484598f9f6eaea74d3bb10b
FTITOR <francisco.ramirez@fr8hub.com>
2021-03-17 21:55:19 -0600

    correccion en app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterModule } from './footer/footer.module';
-import { HomeComponent } from './home/home.component';
-import { HomeModule } from './home/home.module';
 import { NavbarModule } from './navbar/navbar.module';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserListComponent,
-    UserDetailComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
