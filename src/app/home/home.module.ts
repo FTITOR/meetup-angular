9a28fad0372a03a988faa940bead9ee950cca1a5
FTITOR <francisco.ramirez@fr8hub.com>
2021-03-17 21:55:48 -0600

    home component routes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
+    HomeComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule.forChild(ROUTES)
   ],
   exports: [
   ]
