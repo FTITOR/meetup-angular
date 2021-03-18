5576626f95f76f46c0d0814ab66ed4fe5cef0cdd
FTITOR <francisco.ramirez@fr8hub.com>
2021-03-17 21:56:08 -0600

    user list component routes

--- src/app/user-list/user-list.module.ts
+++ src/app/user-list/user-list.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+  { path: '', component: UserListComponent }
+];
 
 @NgModule({
   declarations: [
+    UserListComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule.forChild(ROUTES)
   ]
 })
 export class UserListModule { }
