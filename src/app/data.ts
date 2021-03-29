e91bc57cd87b5e7922177f093cb5451bffaa34e5
FTITOR <francisco.ramirez@fr8hub.com>
2021-03-28 22:13:25 -0600

    data test

---
+++ src/app/data.ts
@@ -0,0 +1,24 @@
+import { UserDetail } from "./user-detail/user-detail.model";
+
+export const USERS: Array<UserDetail> = [
+    {id: 1, name: 'FTITOR', lastname: 'RMX', email: 'test@test.com', age: 18},
+    {id: 2, name: 'CEPILLIN', lastname: 'EN LA FERIA', email: 'test1@test.com', age: 75},
+    {id: 3, name: 'ROGER', lastname: 'WATERS', email: 'test2@test.com', age: 72},
+    {id: 4, name: 'ALAN', lastname: 'TURING', email: 'test3@test.com', age: 50},
+    {id: 5, name: 'PEPITO', lastname: 'TEST', email: 'test4@test.com', age: 7},
+    {id: 6, name: 'JUAN', lastname: 'RMX', email: 'test@test.com', age: 11},
+    {id: 7, name: 'DANIEL', lastname: 'EN LA FERIA', email: 'test1@test.com', age: 13},
+    {id: 8, name: 'ANDRIY', lastname: 'WATERS', email: 'test2@test.com', age: 24},
+    {id: 9, name: 'HELI', lastname: 'TURING', email: 'test3@test.com', age: 51},
+    {id: 10, name: 'MIKE', lastname: 'TEST', email: 'test4@test.com', age: 27},
+    {id: 11, name: 'GERARDO', lastname: 'RMX', email: 'test@test.com', age: 28},
+    {id: 12, name: 'ERICK', lastname: 'EN LA FERIA', email: 'test1@test.com', age: 21},
+    {id: 13, name: 'BRANDON', lastname: 'WATERS', email: 'test2@test.com', age: 19},
+    {id: 14, name: 'ENRIQUE', lastname: 'TURING', email: 'test3@test.com', age: 51},
+    {id: 15, name: 'LUIS', lastname: 'TEST', email: 'test4@test.com', age: 72},
+    {id: 16, name: 'ALVARO', lastname: 'RMX', email: 'test@test.com', age: 33},
+    {id: 17, name: 'JORGE', lastname: 'EN LA FERIA', email: 'test1@test.com', age: 43},
+    {id: 18, name: 'PACO', lastname: 'WATERS', email: 'test2@test.com', age: 40},
+    {id: 19, name: 'TOÑO', lastname: 'TURING', email: 'test3@test.com', age: 39},
+    {id: 20, name: 'CHELO', lastname: 'TEST', email: 'test4@test.com', age: 13}
+];
