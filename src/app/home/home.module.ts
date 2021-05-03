14a088ae0b49f168499060eb809c89e7deb59fbe
FTITOR <francisco.ramirez@fr8hub.com>
2021-05-02 21:12:04 -0500

    correcciones en las importaciones de las directivas

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HighlightDirectiveModule } from '../directives/highlight.directive.module';
+import { HighlightDirectiveModule } from '../directives/highlight/highlight.directive.module';
 
 // Definición de rutas "CHILD" para cargar especificamente el componente HomeComponent.
 const ROUTES: Routes = [
