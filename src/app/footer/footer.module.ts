4650bfa37265bbd5f2d7ddf75379ef98a05d324e
FTITOR <francisco.ramirez@fr8hub.com>
2021-05-02 21:07:31 -0500

    implementar el uso de la directiva en el footer

--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
+import { HighlightDirectiveModule } from '../directives/highlight.directive.module';
 
 @NgModule({
   declarations: [
     FooterComponent // COMPONENT -> Declaración del component perteneciente al module (FooterComponent).
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HighlightDirectiveModule
   ],
   exports: [
     FooterComponent // COMPONENT -> Exportar FooterComponent para poder usarlo en otros modules.
