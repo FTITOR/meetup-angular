7a312ad38c024db769b7cb46bfd77f84318b7df7
FTITOR <francisco.ramirez@fr8hub.com>
2021-05-16 21:05:40 -0500

    correccion en la directiva upper case

--- src/app/directives/uppercase/uppercase.directive.ts
+++ src/app/directives/uppercase/uppercase.directive.ts
@@ -6,14 +6,22 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class UppercaseDirective {
 
-  constructor(private el: ElementRef) {}
+  private _lastValue: string;
+
+  constructor(private _el: ElementRef) {}
 
   @HostListener('input', ['$event']) onInput(event) {
-    const initialValue = this.el.nativeElement.value;
+    const start = event.target.selectionStart;
+    const end = event.target.selectionEnd;
     event.target.value = event.target.value.toUpperCase();
+    event.target.setSelectionRange(start, end);
+    event.preventDefault();
 
-    if (initialValue !== this.el.nativeElement.value) {
-      event.stopPropagation();
+    if (!this._lastValue || (this._lastValue && event.target.value.length > 0 && this._lastValue !== event.target.value)) {
+      this._lastValue = this._el.nativeElement.value = event.target.value;
+      const evt = document.createEvent('HTMLEvents');
+      evt.initEvent('input', false, true);
+      event.target.dispatchEvent(evt);
     }
   }
 }
