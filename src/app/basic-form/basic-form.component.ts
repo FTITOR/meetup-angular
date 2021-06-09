b7fcca31d609f6cfd883ed7b29ba18a8f26d148f
FTITOR <francisco.ramirez@fr8hub.com>
2021-06-08 21:41:29 -0500

    form group y form control

--- src/app/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -12,10 +12,21 @@ export class BasicFormComponent implements OnInit {
   email = new FormControl('', [Validators.email]);
   message = new FormControl('Hola escriba su mensaje', [Validators.minLength(2), Validators.maxLength(30)]);
 
+  form: FormGroup;
+
   constructor() {
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.minLength(2), Validators.maxLength(20)]),
+      email: new FormControl('', [Validators.email]),
+      message: new FormControl('Hola escriba su mensaje', [Validators.minLength(2), Validators.maxLength(30)])
+    });
   }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    console.log(this.form.value);
   }
 
 }
